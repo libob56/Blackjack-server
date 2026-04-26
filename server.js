75dfbd3701d8d4148a1c631a4c3e5fe54b467e6b
libob56 <gemiji56@gmail.com>
2026-04-26 16:18:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,23 +4,18 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: { origin: "*" }
-});
+const io = new Server(server, { cors: { origin: "*" } });
 
-// Это чтобы при заходе по ссылке не было ошибки 500
+// Этот блок ОБЯЗАТЕЛЕН для Render, чтобы не было ошибки 500
 app.get('/', (req, res) => {
   res.send('Сервер Блэкджека запущен и готов!');
 });
 
 io.on('connection', (socket) => {
-  console.log('Игрок вошел:', socket.id);
-  socket.on('action', (data) => {
-    console.log('Ход игрока:', data);
-  });
+  console.log('Игрок подключился:', socket.id);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
-  console.log('Работаем на порту:', PORT);
+  console.log('Сервер работает на порту:', PORT);
 });
